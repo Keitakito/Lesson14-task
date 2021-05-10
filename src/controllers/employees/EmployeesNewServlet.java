9aa414e597f8b3a3f6f99cd5d0fc2857cb0fec82
akito takeda <kadokei831@icloud.com>
2021-05-10 14:22:50 +0900

    Add password encryption

--- src/controllers/employees/EmployeesNewServlet.java
+++ src/controllers/employees/EmployeesNewServlet.java
@@ -1,4 +1,4 @@
-package models;
+package controllers.employees;
 
 import java.io.IOException;
 
@@ -8,6 +8,8 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import models.Employee;
 /**
  * Servlet implementation class EmployeesNewServlet
  */
